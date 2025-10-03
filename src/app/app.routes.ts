abb9f2537223e9487c404972f545aa8c1c4e1d63
LucaFerrantedev <lucafer19@gmail.com>
2025-10-02 21:24:56 -0300

    Creada actividades, hay un problema en acceder. Tira cannot get a menos que se comete la linea 36 en app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ContainerComponent } from './components/container/container';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ActividadesComponent } from './components/actividades/actividades.component';
 
 export const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
@@ -27,6 +28,14 @@ export const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       { path: "", component: DashboardComponent }
+    ]
+  },
+  {
+    path: "actividades",
+    component: ContainerComponent,
+    //canActivate: [AuthGuard],
+    children: [
+      { path: "", component: ActividadesComponent }
     ]
   }
 ];
---
+++ src/app/components/actividades/actividades.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar";
+
+@Component({
+  selector: 'app-actividades',
+  imports: [NavbarComponent],
+  templateUrl: './actividades.component.html',
+  styleUrl: './actividades.component.css'
+})
+export class ActividadesComponent {
+
+}
