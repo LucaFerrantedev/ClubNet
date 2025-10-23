84074269b127cb451fb5bfdb165dc69b73794a13
Gloria Fulguera <gloriafulguera@gmail.com>
2025-10-23 20:23:45 -0300

    Cambios para mostrar en HOME solo las actividades cargadas en sistema

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../../services/home.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  DataSource:any;
   constructor(private homeService:HomeService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.GetActividades();
+  }
 
   Continuar() {
     this.router.navigate(['/login']);
+  }
+
+  GetActividades(){
+    this.homeService.GetActividades().subscribe(x=>{
+      this.DataSource=x;
+      console.log(this.DataSource);
+    })
   }
 }
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -6,10 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class HomeService {
   url = "https://localhost:4200/login";
+  urlActividades="https://localhost:7121/api/actividad";
   
   constructor(private httpClient:HttpClient) { }
 
    Continuar() {
      return this.httpClient.get(this.url + "/Continuar");
+   }
+
+   GetActividades(){
+    return this.httpClient.get(this.urlActividades+"/GetActividades")
    }
 }
