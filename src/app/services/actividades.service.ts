58ec6a55cc11a1d5d094b85bac38f3e87c75debf
Gloria Fulguera <gloriafulguera@gmail.com>
2025-10-30 00:30:43 -0300

    Agrego funcionalidad a la pantalla de inscripciones

--- src/app/services/actividades.service.ts
+++ src/app/services/actividades.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginService } from './login.service';
-import { environment } from '../../enviroments/enviroment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
